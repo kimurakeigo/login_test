d703a2cfdf63e23a46f59efe48c2d5d899ec6b55
kk <onpuyumi@gmail.com>
2025-03-06 17:08:29 +0900

    035

--- login_test.py
+++ login_test.py
@@ -311,9 +311,19 @@ def main():
     
     if not st.session_state.authenticated:
         st.sidebar.header(" ログイン")
-        login_method = st.sidebar.radio("ログイン方法を選択してください", ("パスワード", "カメラ認証"))
+        login_method = st.sidebar.radio("ログイン方法を選択してください", ("ユーザー名とパスワード", "カメラ認証"))
 
-        if login_method == "カメラで撮影":
+        if login_method == "ユーザー名とパスワード":
+            email = st.sidebar.text_input(" ユーザー名")
+            password = st.sidebar.text_input(" パスワード", type="password")
+            if st.sidebar.button("ログイン", use_container_width=True):
+                if authenticate_email_password(email, password):
+                    st.session_state.authenticated = True
+                    st.sidebar.success("✅ ログイン成功！")
+                    st.rerun()
+                else:
+                    st.sidebar.error("❌ ログイン失敗")
+        else:
             uploaded_image = st.sidebar.camera_input("カメラで撮影")
             if st.sidebar.button("ログイン", use_container_width=True):
                 email = authenticate_face(uploaded_image)
@@ -321,16 +331,6 @@ def main():
                     st.session_state.authenticated = True
                     st.session_state.user_email = email  # ユーザーのEmailをセッションに保存
                     st.sidebar.success(f"✅ ログイン成功！")  # Emailを表示
-                    st.rerun()
-                else:
-                    st.sidebar.error("❌ ログイン失敗")
-        else:
-            email = st.sidebar.text_input(" ユーザー名")
-            password = st.sidebar.text_input(" パスワード", type="password")
-            if st.sidebar.button("ログイン", use_container_width=True):
-                if authenticate_email_password(email, password):
-                    st.session_state.authenticated = True
-                    st.sidebar.success("✅ ログイン成功！")
                     st.rerun()
                 else:
                     st.sidebar.error("❌ ログイン失敗")
